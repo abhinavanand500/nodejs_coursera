0c40d40a64deaea81bd97411375e1a9924a88471
abhinavanand500 <abhinav.anand500@gmail.com>
2020-05-30 22:43:43 +0530

    routes parameter and assignment 1

--- node-express/index.js
+++ node-express/index.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
+
 const hostname = 'localhost';
 const port = 3000;
-
 const app = express();
+app.use(morgan('dev'));
+app.use('/dishes', dishRouter);
+app.use('/promo', promoRouter);
+app.use('/leader', leaderRouter);
+app.use(express.static(__dirname+ '/public'));
 app.use((req,res,next) =>{
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Context-Type', 'text/html');
     res.end('<html><body><h1>Hello World</h1></body></html>');
 });
-
-
-
 const server = http.createServer(app);
-server.listen(port,hostname,()=>{
+app.listen(port,hostname,()=>{
     console.log(`Server running at http://${hostname}:${port}`);
 });
